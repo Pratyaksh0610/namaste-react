376b2ab163e4ad0ae62a9b496d7726a298bcbb25
Pratyaksh <pratyaksh0610@gmail.com>
2026-03-20 10:28:16 +0530

    learning-react-1

---
+++ basics/App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {
+    id: "HELLO_BROTHER"
+}, "HELLO FROM REACTJS");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
