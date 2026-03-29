dddc6281598aa621c40bebb46d56a552784dbf31
Pratyaksh0610 <pratyaksh0610@gmail.com>
2026-03-29 20:11:53 +0530

    ep 2: igniting app - bundler,npm,package.json

--- basics/App.js
+++ basics/App.js
@@ -1,5 +1,11 @@
-const heading = React.createElement("h1", {
-    id: "HELLO_BROTHER"
-}, "HELLO FROM REACTJS");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", {}, [
+  React.createElement("h1", {}, [
+    "NEW START",
+    React.createElement("p", {}, "NEW END"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
